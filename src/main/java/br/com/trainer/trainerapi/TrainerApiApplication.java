fe635be63c8890f06718f66656518a7991251587
mftech <matheussilvafeitosa78@gmail.com>
2022-08-08 19:03:06 -0300

    generate project

---
+++ src/main/java/br/com/trainer/trainerapi/TrainerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.trainer.trainerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trainer/trainerapi/TrainerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trainer.trainerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
